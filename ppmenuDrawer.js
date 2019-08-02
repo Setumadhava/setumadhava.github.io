eaf3b12e6731dbdbb7507b502fde248dc41a23b2
setumad <setu.gk@gmail.com>
2019-08-02 18:48:03 -0400

    project header solid!!!

---
+++ ppmenuDrawer.js
@@ -0,0 +1,16 @@
+document.getElementById("ppmenuicon").onclick = function() {
+    document.getElementById("ppmenuicon").style.opacity = 0;
+    document.getElementById("ppmenuicon").style.zIndex = -2;
+    document.getElementById("ppmobilemenuwrapper").style.opacity = 1;
+    document.getElementById("ppmobilemenuwrapper").style.zIndex = 2;
+}
+document.getElementById('menuxicon').onclick = function() {
+
+    document.getElementById("ppmobilemenuwrapper").style.opacity = 0;
+    document.getElementById("ppmobilemenuwrapper").style.zIndex = -2;
+    setTimeout(function() {
+        document.getElementById("ppmenuicon").style.opacity = 1;
+        document.getElementById("ppmenuicon").style.zIndex = 2;
+    }, 150);
+
+}
