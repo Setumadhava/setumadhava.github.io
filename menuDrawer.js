900dbe348ebda50e6801c5f452798224c6fd2c05
setumad <setu.gk@gmail.com>
2019-08-02 18:06:50 -0400

    Navigation - Solid!!

--- menuDrawer.js
+++ menuDrawer.js
@@ -1,11 +1,11 @@
 document.getElementById("menuicon").onclick = function() {
     document.getElementById("menuicon").style.opacity = 0;
     document.getElementById("menuicon").style.zIndex = -2;
-    document.getElementById("mobilemenuwrapper").style.opacity = 1;
+    document.getElementById("mobilemenuwrapper").style.opacity = 01;
     document.getElementById("mobilemenuwrapper").style.zIndex = 2;
 }
 document.getElementById('menuxicon').onclick = function() {
-    console.log("hi");
+
     document.getElementById("mobilemenuwrapper").style.opacity = 0;
     document.getElementById("mobilemenuwrapper").style.zIndex = -2;
     setTimeout(function() {
