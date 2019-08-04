607f77b3ce15060fc6aa515b7b872e2b920771b4
setumad <setu.gk@gmail.com>
2019-08-04 11:07:39 -0400

    js obfuscated

---
+++ Restricted JS/restricted.js
@@ -0,0 +1,47 @@
+
+
+//Create a variable with the link for each webpage you want to hide access to.
+
+var ncv = "http://setumadhava.com/thd_noncon_dashboard.html";
+var index = "http://setumadhava.com/index19.html";
+
+//Include the variable in the pahesList list and create a password for the page. They must be in the same position in their respective lists.
+
+var pagesList = [index, ncv];
+var passwordList = ['index', 'ZimaBlue'];
+
+document.getElementById("inputBox").onclick = function() {
+    document.getElementById("worngpwdiv").style.visibility = "hidden";
+    document.getElementById("inputBox").style.borderColor = "#7E7E7E";
+    document.getElementById("inputBox").style.borderWidth = "1px";
+}
+
+
+document.getElementById("buttondiv").onclick = function() {
+    var formValue = document.getElementById("inputBox").value;
+
+    for (j = 0; j < passwordList.length; j++) {
+        if (formValue === passwordList[j]) {
+            window.location = pagesList[j];
+            document.getElementById("worngpwdiv").style.visibility = "hidden";
+            document.getElementById("inputBox").style.borderColor = "#7E7E7E";
+            document.getElementById("inputBox").style.borderWidth = "1px";
+            break;
+
+        } else {
+            document.getElementById("worngpwdiv").style.visibility = "visible";
+            document.getElementById("inputBox").style.borderColor = "red";
+            document.getElementById("inputBox").style.borderWidth = "2px";
+            document.getElementById("inputBox").value = "";
+
+            setInterval(
+                function() {
+                    document.getElementById("worngpwdiv").style.visibility = "hidden";
+                    document.getElementById("inputBox").style.borderColor = "#7E7E7E";
+                    document.getElementById("inputBox").style.borderWidth = "1px";
+                },
+                3000
+            );
+        }
+    }
+}
